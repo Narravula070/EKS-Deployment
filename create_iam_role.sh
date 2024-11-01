171c70dafe2809a448489ad4077d9988eb5e5e20
Narravula muni sai teja <124333841+Narravula070@users.noreply.github.com>
2024-11-01 11:21:31 +0530

    Update create_iam_role.sh

--- create_iam_role.sh
+++ create_iam_role.sh
@@ -3,16 +3,16 @@ TRUST="{   \"Version\": \"2012-10-17\",   \"Statement\": [     {       \"Effect\
 
 echo '{ "Version": "2012-10-17", "Statement": [ { "Effect": "Allow", "Action": "eks:Describe*", "Resource": "*" } ] }' > /tmp/iam-role-policy
 
-aws iam create-role --role-name CodeBuildKubectlRole --assume-role-policy-document "$TRUST" --output text --query 'Role.Arn'
+aws iam create-role --role-name CodeBuildEKSDeploymentrole --assume-role-policy-document "$TRUST" --output text --query 'Role.Arn'
 
-aws iam put-role-policy --role-name CodeBuildKubectlRole --policy-name eks-describe --policy-document file:///tmp/iam-role-policy
+aws iam put-role-policy --role-name CodeBuildEKSDeploymentrole --policy-name eks-describe --policy-document file:///tmp/iam-role-policy
 
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
+aws iam attach-role-policy --role-name CodeBuildEKSDeploymentrole --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
 
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess
+aws iam attach-role-policy --role-name CodeBuildEKSDeploymentrole --policy-arn arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess
 
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AWSCodeCommitFullAccess
+aws iam attach-role-policy --role-name CodeBuildEKSDeploymentrole --policy-arn arn:aws:iam::aws:policy/AWSCodeCommitFullAccess
 
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+aws iam attach-role-policy --role-name CodeBuildEKSDeploymentrole --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
+aws iam attach-role-policy --role-name CodeBuildEKSDeploymentrole --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
